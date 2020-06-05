bc53a65990037a4f91ac068c21e46b079faaf822
Sofia Vashchenko <sofiavashchenko@Raphs-MacBook.local>
2020-06-05 20:34:37 +0300

    removed .idea directory

---
+++ src/com/lits/second/Palindrome.java
@@ -0,0 +1,10 @@
+package com.lits.second;
+
+public class Palindrome {
+    public static void main(String args[]) {
+        String palindrome = "War saw raw";
+        String len = palindrome.length();
+        System.out.println("String Length is : " + len);
+    }
+}
+}
